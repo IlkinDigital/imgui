1fbf9c357698f3e3681b2bb7f04bc2a2b285c83e
Ilkin Mammadli <81753493+LesterDigital@users.noreply.github.com>
2022-06-21 22:02:19 +0400

    Set staticruntime to "off".

--- premake5.lua
+++ premake5.lua
@@ -2,7 +2,7 @@ project "ImGui"
     kind "StaticLib"
     language "C++"
     cppdialect "C++17"
-    staticruntime "on"
+    staticruntime "off"
 
     targetdir ("../../../Binaries/" .. outputdir .. "/%{prj.name}")
     objdir ("../../../Binaries-Int/" .. outputdir .. "/%{prj.name}")
